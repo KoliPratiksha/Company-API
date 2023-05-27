0b1ae7aa7a2dca15bc5413a130fa403564815c71
KoliPratiksha <130288986+KoliPratiksha@users.noreply.github.com>
2023-05-27 17:12:30 +0530

    relaod upload

--- app.js
+++ app.js
@@ -1,38 +1,33 @@
 require('dotenv/config')
-
-const cors = require('cors')
 const mongoose = require('mongoose')
 const express = require('express')
-const User_route = require('./route/userroute')
-const Hotel_route = require('./route/hotelroute')
-
+const companyRoute = require('./route/companyRoute')
+const cors = require('cors')
 
 const app = express()
 
-// midleware
+// middleware
 app.use(express.json())
 app.use(cors())
 
+app.use('/api/company',companyRoute)
 
-app.use('/api/user',User_route)
-app.use('/api/hotel',Hotel_route)
 
-// default
+// default route
 app.get('/',(req,res)=>{
-    res.send("This is home")
+      res.send("Home");
 })
 
 app.listen(process.env.PORT,()=>{
     console.log("Listening on port 5000");
 })
-
-async function main(){
+async function main () {
     try {
         const res = await mongoose.connect(process.env.DB)
         const data = await res.default
         console.log(data.STATES.connected);
     } catch (error) {
-        console.log(error.message);
+        console.log("error");
     }
 }
 main()
